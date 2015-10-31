22bebdc7cf711ab0d988efde77dd6adadfba2537
eaHat <ea.chatzimichali@googlemail.com>
2015-10-31 12:05:35 +0000

    Extra plotting functions

--- visplots.py
+++ visplots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
 import numpy as np
 
 
@@ -36,7 +37,7 @@ def knnDecisionPlot(XTrain, yTrain, XTest, yTest, n_neighbors, weights):
     plt.title("2-Class classification (k = %i, weights = '%s')" % (n_neighbors, weights))
     plt.show()
 
-def rfDecisionPlot(XTrain, yTrain, XTest, yTest):
+def rfDecisionPlot(XTrain, yTrain, XTest, yTest, n_estimators=10):
     plt.figure(figsize=(7,5))
     h = .02  # step size in the mesh
     Xtrain = XTrain[:, :2] # we only take the first two features.
@@ -45,7 +46,7 @@ def rfDecisionPlot(XTrain, yTrain, XTest, yTest):
     cmap_light = ListedColormap(["#AAAAFF", "#AAFFAA", "#FFAAAA"])
     cmap_bold  = ListedColormap(["#0000FF", "#00FF00", "#FF0000"])
 
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(n_estimators=n_estimators, random_state=1)
     clf.fit(Xtrain, yTrain)
 
     # Plot the decision boundary. For that, we will assign a color to each
@@ -66,4 +67,37 @@ def rfDecisionPlot(XTrain, yTrain, XTest, yTest):
     plt.ylabel('volatile_acidity')
     plt.title("2-Class classification Random Forests")
     plt.show()
+
+def rfAvgAcc(rfModel, XTest, yTest):
+    preds = []
+    avgPred = []
+    df = []
+
+    for i,tree in enumerate(rfModel.estimators_):
+        predTree = tree.predict(XTest)
+        accTree  = round(metrics.accuracy_score(yTest, predTree),2)
+        preds.append(accTree)
+        if i==0:
+            df = predTree
+        else:
+            df = np.vstack((df,predTree))
+
+
+    for j in np.arange(df.shape[0]):
+        j=j+1
+        mv = []
+        for i in np.arange(df.shape[1]):
+            (values,counts) = np.unique(df[:j,i],return_counts=True)
+            ind=np.argmax(counts)
+            mv.append(values[ind].astype(int))
+        avgPred.append(metrics.accuracy_score(yTest, mv))
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(avgPred,  '.', linestyle='-', color='c')
+    plt.ylim(0.8,0.95)
+    plt.xlabel("Number of trees")
+    plt.ylabel("Accuracy")
+    plt.grid()
+    plt.show()
+
 
